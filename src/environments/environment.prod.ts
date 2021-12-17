3df2b978e9508ab7adfe5c560e5f18be0f0280c8
anandmishra13 <kumaranandmishra2001@gmail.com>
2021-12-18 00:22:02 +0530

    lazy loading & 97

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyDgPKBU8t09ADcasFe17n0zvCAjvzjthUw",
+    authDomain: "my-demo-ngrx.firebaseapp.com",
+    projectId: "my-demo-ngrx",
+    storageBucket: "my-demo-ngrx.appspot.com",
+    messagingSenderId: "525889013799",
+    appId: "1:525889013799:web:92b76ab441028ee0062602",
+    measurementId: "G-K5963XZN2V"
+  }
 };
